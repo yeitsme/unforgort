629ddac55ae6a487bd9ea5df02ff4256f00e3b33
hello A <helloA@abc.cn>
2020-10-12 22:57:58 +0800

    增加内容提交

--- src/cn/itcast/service/impl/OneImpl.java
+++ src/cn/itcast/service/impl/OneImpl.java
@@ -3,5 +3,8 @@ package cn.itcast.service.impl;
 public class OneImpl {
     public static void main(String[] args) {
         System.out.println("绿了就是可以提交了");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("zhensgebancui");
+        }
     }
 }
